73247937a63a0693a0d34b1d09335d346e2c060e
trevor <trevorgriarte@gmail.com>
2026-01-18 01:35:57 -0800

    fixing navbar

--- scripts/index.js
+++ scripts/index.js
@@ -11,3 +11,23 @@ function openNav() {
   document.getElementById("content").style.marginLeft = "300px";
   document.querySelector(".card-container").style.marginLeft = "300px";
 }
+
+window.addEventListener("DOMContentLoaded", () => {
+  openNav();
+});
+
+window.addEventListener("DOMContentLoaded", () => {
+  const sidebar = document.getElementById("js-sidebar");
+  const title = document.getElementById("title");
+  const content = document.getElementById("content");
+
+  sidebar.classList.add("no-transition");
+  title.classList.add("no-transition");
+  content.classList.add("no-transition");
+
+  requestAnimationFrame(() => {
+    sidebar.classList.remove("no-transition");
+    title.classList.remove("no-transition");
+    content.classList.remove("no-transition");
+  });
+});
